cce169d7d34d66790c8e37d4c678bbf992589863
RiadHossain43 <riadhossain7464@gmail.com>
2021-03-01 02:43:33 +0600

    Fixed issues

---
+++ assets/js/contactService.js
@@ -0,0 +1,14 @@
+
+function requestDemo(){
+    let personName = $("#person-name").val()
+    let personEmail = $("#person-email").val()
+
+    let personTile = $("#person-title").val()
+    let personOrg = $("#person-org").val()
+
+    
+
+    fetch("https://imssystems.tech",{
+
+    })
+}
